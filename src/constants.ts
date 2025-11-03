0ad9637683884340c70feec0bdd565470c9edab9
berth1721 <112241834+berth1721@users.noreply.github.com>
2025-11-03 19:03:09 +0800

    voucher final 2

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const AMOUNT_PER_STUDENT = 40000;
+export const STIPEND_PER_STUDENT = 35000;
+export const BOOK_ALLOWANCE_PER_STUDENT = 5000;
+
+export const PROGRAM_NAME = "Scholarship Program for Coconut Farmers and their Families (CoScho)";
+export const SCHOOL_YEAR = "2ND SEM AY 2024 - 2025";
+export const PAYEE_NAME = "VARIOUS GRANTEES";
+export const COORDINATOR_NAME = "NILA A. MAGAT";
+export const COORDINATOR_TITLE = "ES II/ StuFAP's Coordinator";
+export const DIRECTOR_NAME = "DR. ROGELIO T. GALERA JR, CESO III";
+export const DIRECTOR_TITLE = "Director IV";
+export const ACCOUNTANT_NAME = "DANICA A. DE SILVA";
+export const ACCOUNTANT_TITLE = "Accountant II";
+export const ACCOUNTANT_SUB_TITLE = "Head, Accounting Unit/Authorized Representative";
